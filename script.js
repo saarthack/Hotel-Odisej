3fa4f43e2d4f14a0aad5dc2902401096c73bfe53
Sarthak Sharma <ssarthak770@gmail.com>
2023-08-07 20:29:54 +0530

    page5 animation done

--- script.js
+++ script.js
@@ -1,4 +1,46 @@
-// Swiper js ka code 
+(function init() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector("#main").style.transform
+      ? "transform"
+      : "fixed",
+  });
+
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
+})();
+
+// IIFE  - Immediately Invoked Function Expression
+
+// Swiper js ka code
 
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: "auto",
@@ -11,8 +53,6 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
-
-
 
 var tl = gsap.timeline();
 tl.from("#page1 svg", {
@@ -52,7 +92,7 @@ gsap.to("#page2 h2 span", {
   stagger: 0.3,
   scrollTrigger: {
     trigger: "#page2 h2 span",
-    scroller: "body",
+    scroller: "#main",
     start: "top 55%",
     end: "top -5%",
     scrub: 2,
@@ -63,8 +103,31 @@ gsap.to("#page2 #svg2,#page2 #svg3", {
   left: "-100vw",
   scrollTrigger: {
     trigger: "#page2 #svg2",
-    scroller: "body",
-    markers: true,
+    scroller: "#main",
     scrub: 2,
   },
 });
+
+var tl2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: "#page5",
+    scroller: "#main",
+    markers: true,
+    start: "top 60%",
+    end: "top 40%",
+    scrub: 3,
+  },
+});
+tl2.to("#page5-left", {
+  transform: `translateX(-50%)`,
+  duration: 1,
+},"page5-anim");
+tl2.to("#page5-right", {
+  transform: `translateX(50%)`,
+  duration: 1,
+},"page5-anim");
+tl2.from("#page5-center", {
+  transform: `translateY(10%)`,
+  duration: 1,
+  opacity:0,
+},"page5-anim");
